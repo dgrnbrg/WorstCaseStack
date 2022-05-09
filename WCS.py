9ec9c2d357326a97bbea82012f4d47c12686c189
David Greenberg <dsg123456789@gmail.com>
2022-05-09 17:44:45 -0400

    Working to show the causal trace of stacked calls in RTOS functions, updated to gcc 10

--- WCS.py
+++ WCS.py
@@ -12,7 +12,7 @@ dir = None # Working directory
 su_ext = '.su'
 obj_ext = '.o'
 manual_ext = '.msu'
-read_elf_path = "arm-none-eabi-readelf.exe" # You may need to enter the full path here
+read_elf_path = "arm-none-eabi-readelf" # You may need to enter the full path here
 stdout_encoding = "utf-8"  # System dependant
 
 
@@ -33,7 +33,7 @@ def read_symbols(file):
 
         s2 = Symbol()
         s2.value = int(v[1], 16)
-        s2.size = int(v[2])
+        s2.size = int(v[2]) if not v[2].startswith('0x') else int(v[2], 16)
         s2.type = v[3]
         s2.binding = v[4]
         if len(v) >= 8:
@@ -128,9 +128,13 @@ def read_rtl(tu, call_graph):
 
     # Construct A Call Graph
     function = re.compile(r'^;; Function (.*) \((\S+), funcdef_no=\d+(, [a-z_]+=\d+)*\)( \([a-z ]+\))?$')
-    static_call = re.compile(r'^.*\(call.*"(.*)".*$')
-    other_call = re.compile(r'^.*call .*$')
+    static_call = re.compile(r'^.*\(expr_list:REG_CALL_DECL.*"(.*)".*$')
+    other_call = re.compile(r'^.*[^(]call .*$') # note that we skip the (call lines
 
+    print(f'processing rtl file: {tu + rtl_ext}')
+    # TODO this rtl parser sucks & doesn't actually work. need to really split by line or sexpr, and I'm not sure that it's even parsing calls correctly
+    #      (expr_list:REG_CALL_DECL (symbol_ref:SI ("alloc_can_msg") [flags 0x41]  <function_decl 0x1089def00 alloc_can_msg>) might be a better type of line to match on
+    #                (call (mem:SI (reg/f:SI 9 r9 [550]) [0 alloc_can_msg S4 A32]) is another way to match, which just has us changing the static call match
     for line_ in open(tu + rtl_ext).readlines():
         m = function.match(line_)
         if m:
@@ -155,7 +159,6 @@ def read_rtl(tu, call_graph):
         if m:
             fxn_dict2['has_ptr_call'] = True
             continue
-
 
 def read_su(tu, call_graph):
     """
@@ -209,7 +212,8 @@ def validate_all_data(call_graph):
     def validate_dict(d):
         if not ('calls' in d and 'has_ptr_call' in d and 'local_stack' in d
                 and 'name' in d and 'tu' in d):
-            print("Error data is missing in fxn dictionary {}".format(d))
+            if not d['name'].endswith('_Handler'):
+                print("Error data is missing in fxn dictionary {}".format(d))
 
     # Loop through every global and local function
     # and resolve each call, save results in r_calls
@@ -225,6 +229,10 @@ def resolve_all_calls(call_graph):
         fxn_dict2['r_calls'] = []
         fxn_dict2['unresolved_calls'] = set()
 
+        if 'calls' not in fxn_dict2:
+            if 'is_weak' not in fxn_dict2 or not fxn_dict2['is_weak']:
+                raise Exception(f'no calls in {fxn_dict2}')
+            return
         for call in fxn_dict2['calls']:
             call_dict = find_fxn(fxn_dict2['tu'], call, call_graph)
             if call_dict:
@@ -252,22 +260,38 @@ def calc_all_wcs(call_graph):
         :return:
         """
 
+        # if local stack not known, this is probably the weak alias types that need implementing
+        if 'local_stack' not in fxn_dict2:
+            if not fxn_dict2['name'].endswith('_Handler'):
+                print(f'skipping missing stack: {fxn_dict2}')
+            fxn_dict2['wcs'] = 0
+            return
+
         # If the wcs is already known, then nothing to do
         if 'wcs' in fxn_dict2:
             return
 
-        # Check for pointer calls
+        # Check for pointer calls (dg: and ignore because of their commonness)
         if fxn_dict2['has_ptr_call']:
-            fxn_dict2['wcs'] = 'unbounded'
-            return
+            #raise Exception(f'Found ptr call, oddly: {fxn_dict2}')
+            #fxn_dict2['wcs'] = 'unbounded-ptr'
+            #return
+            fxn_dict2['wcs-ptr'] = True
 
         # Check for recursion
         if fxn_dict2 in parents:
-            fxn_dict2['wcs'] = 'unbounded'
+            #raise Exception(f'Found recursion, oddly: {fxn_dict2}')
+            #fxn_dict2['wcs'] = 'unbounded-recursion'
+            #return
+            fxn_dict2['wcs-recursion'] = True
+            fxn_dict2['wcs'] = fxn_dict2['local_stack']
             return
 
         # Calculate WCS
         call_max = 0
+        #if len(fxn_dict2['r_calls']) > 0:
+        #    pprint.pprint(fxn_dict2)
+        child_max = None
         for call_dict in fxn_dict2['r_calls']:
 
             # Calculate the WCS for the called function
@@ -276,18 +300,39 @@ def calc_all_wcs(call_graph):
             parents.pop()
 
             # If the called function is unbounded, so is this function
-            if call_dict['wcs'] == 'unbounded':
-                fxn_dict2['wcs'] = 'unbounded'
-                return
+            #if 'unbounded' in call_dict['wcs']:
+            #    #raise 'lol'
+            #    fxn_dict2['wcs'] = f'unbounded-{call_dict["wcs"]}'
+            #    return
+            if 'wcs-recursion' in call_dict:
+                fxn_dict2['wcs-recursion'] = True
+            if 'wcs-ptr' in call_dict:
+                fxn_dict2['wcs-ptr'] = True
 
             # Keep track of the call with the largest stack use
-            call_max = max(call_max, call_dict['wcs'])
+            if call_dict['wcs'] > call_max:
+                call_max = call_dict['wcs']
+                child_max = call_dict['name']
 
             # Propagate Unresolved Calls
             for unresolved_call in call_dict['unresolved_calls']:
                 fxn_dict2['unresolved_calls'].add(unresolved_call)
 
         fxn_dict2['wcs'] = call_max + fxn_dict2['local_stack']
+        fxn_dict2['wcs_child'] = child_max
+        fxn_dict2['wcs_path'] = []
+        child_fxn = fxn_dict2
+        while child_max is not None:
+            child_fxn = find_fxn(child_fxn['tu'], child_max, call_graph)
+            child_local_stack = child_fxn['local_stack'] if child_fxn else -1
+            fxn_dict2['wcs_path'].append((child_max,child_local_stack))
+            if child_fxn is not None and 'wcs_child' in child_fxn:
+                child_max = child_fxn['wcs_child']
+            else:
+                child_max = None
+            if child_max in [x[0] for x in fxn_dict2['wcs_path']]:
+                fxn_dict2['wcs_path'].append((-1, f'recurse={child_max}'))
+                child_max = None
 
     # Loop through every global and local function
     # and resolve each call, save results in r_calls
@@ -306,15 +351,23 @@ def print_all_fxns(call_graph):
         stack = str(fxn_dict2['wcs'])
         if unresolved:
             unresolved_str = '({})'.format(' ,'.join(unresolved))
-            if stack != 'unbounded':
+            if 'unbounded' not in stack:
                 stack = "unbounded:" + stack
         else:
             unresolved_str = ''
+        if 'wcs-recursion' in fxn_dict2:
+            stack += ' recur'
+        if 'wcs-ptr' in fxn_dict2:
+            stack += ' ptr'
+        #    pprint.pprint(fxn_dict2)
 
-        print(row_format.format(fxn_dict2['tu'], fxn_dict2['demangledName'], stack, unresolved_str))
+        if fxn_dict2['demangledName'] in ['rtos_can_recver', 'rtos_can_irq_monitor', 'rtos_can_sender', 'gait_controller_task', 'adc_conversion_publisher', 'safety_task', 'safety_task_can_publisher', 'rtos_servo_tx_task', 'rtos_servo_rx_task', 'max77960_driver', 'max77960_visualization', 'led_dither_driver_task', 'spot_render_task'] or fxn_dict2['demangledName'].endswith('_Handler'):
+            print(row_format.format(fxn_dict2['tu'], fxn_dict2['demangledName'], 'self=' + str(fxn_dict2['local_stack']), stack, unresolved_str))
+            child_calls = [c for c in fxn_dict2["wcs_path"]]
+            print(f'   child calls: {child_calls}')
 
     def get_order(val):
-        if val == 'unbounded':
+        if isinstance(val,str):
             return 1
         else:
             return -val
@@ -323,23 +376,27 @@ def print_all_fxns(call_graph):
     # and resolve each call, save results in r_calls
     d_list = []
     for fxn_dict in call_graph['globals'].values():
-        d_list.append(fxn_dict)
+        if 'wcs' in fxn_dict:
+           d_list.append(fxn_dict)
 
     for l_dict in call_graph['locals'].values():
         for fxn_dict in l_dict.values():
-            d_list.append(fxn_dict)
+            if 'wcs' in fxn_dict:
+                d_list.append(fxn_dict)
 
-    d_list.sort(key=lambda item: get_order(item['wcs']))
+    d_list.sort(key=lambda item: get_order(item['wcs']), reverse=True)
 
     # Calculate table width
     tu_width = max(max([len(d['tu']) for d in d_list]), 16)
     name_width = max(max([len(d['name']) for d in d_list]), 13)
-    row_format = "{:<" + str(tu_width + 2) + "}  {:<" + str(name_width + 2) + "}  {:>14}  {:<17}"
+    row_format = "{:<" + str(tu_width + 2) + "}  {:<" + str(name_width + 2) + "} {:>8} {:>14}  {:<17}"
 
     # Print out the table
     print("")
-    print(row_format.format('Translation Unit', 'Function Name', 'Stack', 'Unresolved Dependencies'))
+    print(row_format.format('Translation Unit', 'Function Name', 'Local', 'Stack', 'Unresolved Dependencies'))
     for d in d_list:
+        if 'local_stack' not in d or d['wcs'] == 0:
+            continue # TODO dg: probably the weak alias handlers
         print_fxn(row_format, d)
 
 
@@ -365,7 +422,9 @@ def find_files():
     all_files = []
     for root, directories, filenames in os.walk('.'):
         for filename in filenames:
-            all_files.append(os.path.join(root,filename))
+            p = os.path.join(root,filename)
+            if 'tinyusb' not in p and 'bootloader' not in p and not 'pb-c' in p and not 'protobuf' in p:
+                all_files.append(p)
 
     files = [f for f in all_files if os.path.isfile(f) and f.endswith(rtl_ext)]
     for f in files:
@@ -403,6 +462,7 @@ def main():
         
     for fxn in call_graph['weak'].values():
         if fxn['name'] not in call_graph['globals'].keys():
+            fxn['is_weak'] = True
             call_graph['globals'][fxn['name']] = fxn
 
     for tu in tu_list:
